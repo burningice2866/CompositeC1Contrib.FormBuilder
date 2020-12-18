1336740e6aff02c2618154ea2b0fc2ff76db3957
Pauli Østerø <pauli@burningice.dk>
2020-12-18 16:35:08 +0100

    Bump version to 0.14.0, contains breaking change regarding WebApi dependency

--- FormBuilder.Dynamic/Properties/AssemblyInfo.cs
+++ FormBuilder.Dynamic/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("BurningIce")]
 [assembly: AssemblyProduct("CompositeC1Contrib.FormBuilder.Dynamic")]
-[assembly: AssemblyCopyright("Copyright © BurningIce 2013")]
+[assembly: AssemblyCopyright("Copyright © BurningIce 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.13.0")]
-[assembly: AssemblyFileVersion("0.13.0")]
+[assembly: AssemblyVersion("0.14.0")]
+[assembly: AssemblyFileVersion("0.14.0")]

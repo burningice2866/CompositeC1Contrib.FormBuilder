b5747c6eb05d355d27f15fb8c80cfdb4f4aa9730
Pauli Østerø <pauli@burningice.dk>
2020-12-18 16:25:51 +0100

    Helper extension method to get the associated Poco instance from a loosly typed Form object

---
+++ FormBuilder.POCO/FormExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CompositeC1Contrib.FormBuilder
+{
+    public static class FormExtensions
+    {
+        public static T ToPoco<T>(this Form form) where T : class
+        {
+            if (!form.FormData.TryGetValue("PocoInstance", out var instance))
+            {
+                throw new ArgumentException("No Poco associated with the form");
+            }
+
+            var poco = instance as T;
+            if (poco == null)
+            {
+                throw new InvalidOperationException("Poco was not of the expected type");
+            }
+
+            return poco;
+        }
+    }
+}
